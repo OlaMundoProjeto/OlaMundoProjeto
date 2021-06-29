fd101ba9b7ec6cfa2630bd6fdfb6f8c23a85ac1d
Fernando Caruso <luisfernandocaruso95@gmail.com>
2021-06-29 12:15:05 -0300

    Projeto Spring - Task 3

---
+++ Back-End/Task 3_ProjetoSpring/ProjetoOlaMundo/src/main/java/br/org/generation/projetointegrador/ProjetoOlaMundoApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOlaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOlaMundoApplication.class, args);
+	}
+
+}
