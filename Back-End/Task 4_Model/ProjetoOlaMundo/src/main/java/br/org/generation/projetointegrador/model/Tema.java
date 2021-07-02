bee4c191a1d619d4d8828b4c223d69a92df47658
Rafael <rafaeldealmeida.192001@gmail.com>
2021-07-02 10:59:19 -0300

    Mudança na tabela Tema.

---
+++ Back-End/Task 4_Model/ProjetoOlaMundo/src/main/java/br/org/generation/projetointegrador/model/Tema.java
@@ -0,0 +1,67 @@
+package br.org.generation.projetointegrador.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 15)
+	private String temaPostagem;
+	
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String ambientacao; // area interna e externa
+	
+	@NotNull
+	@Size(min = 2, max = 15)
+	private String palavra_chave;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTemaPostagem() {
+		return temaPostagem;
+	}
+
+	public void setTemaPostagem(String temaPostagem) {
+		this.temaPostagem = temaPostagem;
+	}
+
+	public String getAmbientacao() {
+		return ambientacao;
+	}
+
+	public void setAmbientacao(String ambientacao) {
+		this.ambientacao = ambientacao;
+	}
+
+	public String getPalavra_chave() {
+		return palavra_chave;
+	}
+
+	public void setPalavra_chave(String palavra_chave) {
+		this.palavra_chave = palavra_chave;
+	}
+	
+	
+	
+
+
+}
